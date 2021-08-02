34a2d40cd8be61c5cd8d07ffa632c65e9ce5940f
davidfoigel <72942152+davidfoigel@users.noreply.github.com>
2021-08-02 12:04:51 -0300

    bosque ponele

--- proyecto posta/Assets/spotMonologo.cs
+++ proyecto posta/Assets/spotMonologo.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 public class spotMonologo : MonoBehaviour
 {
     public int id;
+    public ReadTxt script;
+    public bool[] ids = {true, true, true};
     public GameObject dialogueManager;
+
+    void Awake(){
+        dialogueManager = GameObject.Find("Dialogue Manager");
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            //dialogueManager.GetComponent<ReadTxt>().StartDialogue("MonologoBosque.txt", 1,2);
+            script = dialogueManager.GetComponent<ReadTxt>();
+            if(id == 1 && ids[0] && this.name == "Spot Monologo 1"){
+                Debug.Log("Aca");
+                script.StartDialogue("Bosque.txt.txt", 0, 6);
+                ids[0] = false;
+            }
+            if(id == 2 && ids[1] && this.name == "Spot Monologo 2"){
+                script.setEstaPresente(true);
+                ids[1] = false;
+            }
+            if(id == 3 && ids[2] && this.name == "Spot Monologo 3"){
+                script.setEstaPresente(true);
+                ids[2] = false;
+            }
         }
     }
 }
