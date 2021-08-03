7f434600bf496bfe803515ee5c181d25612b37ae
ianco99 <71154684+ianco99@users.noreply.github.com>
2021-08-03 00:44:56 -0300

    estructura instituto de arte

--- proyecto posta/Assets/Ian/Scripts/Player Scripts/Sprite.cs
+++ proyecto posta/Assets/Ian/Scripts/Player Scripts/Sprite.cs
@@ -6,6 +6,8 @@ public class Sprite : MonoBehaviour
 {
     void LateUpdate()
     {
-        this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, this.transform.parent.rotation.z * -1.0f);
+        transform.forward = new Vector3(Camera.main.transform.forward.x, transform.forward.y, Camera.main.transform.forward.z);
+        //transform.LookAt(Camera.main.transform.position, Vector3.up);
+        //this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, this.transform.parent.rotation.z * -1.0f);
     }
 }
