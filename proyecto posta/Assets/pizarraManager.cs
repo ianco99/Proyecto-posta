611719de0ba69522437157a55ab16fce461934ea
ianco99 <71154684+ianco99@users.noreply.github.com>
2021-08-14 00:11:57 -0300

    FADE IN PIZARRA

--- proyecto posta/Assets/pizarraManager.cs
+++ proyecto posta/Assets/pizarraManager.cs
@@ -10,8 +10,8 @@ public class pizarraManager : Interactable1
     int movimiento;
     public float fuerza;
     [SerializeField] GameObject student;
-    [SerializeField] float startTime;
-    float currentTime = 0;
+    [SerializeField] float fadeOutTime = 1f;
+    [SerializeField] SpriteRenderer sprite;
 
     private void Start()
     {
@@ -23,11 +23,7 @@ public class pizarraManager : Interactable1
 
         
         on = !on;
-        while (on)
-        {
-
-            Dibujo(on);
-        }
+        StartCoroutine(Dibujo(sprite, on));
         student.gameObject.SetActive(false);
     }
 
@@ -36,16 +32,22 @@ public class pizarraManager : Interactable1
         return "Apreta la " + "E" + " para apagar/prender la luz.";
     }
 
-    void Dibujo(bool on)
+    IEnumerator Dibujo ( SpriteRenderer _sprite, bool isOn)
     {
-        
-            if(currentTime > startTime)
-            {
-                Debug.Log("moviendo pizarra ayeee");
-                on = !on;
-            }
-            currentTime += Time.deltaTime;
+            Color tmpColor = _sprite.color;
 
-        Debug.Log("Terminó");
+            while (tmpColor.a < 1f)
+            {
+                tmpColor.a += Time.deltaTime / fadeOutTime;
+                _sprite.color = tmpColor;
+
+                if (tmpColor.a >= 1f)
+                {
+                    tmpColor.a = 1.0f;
+                }
+                yield return null;
+            }
+            _sprite.color = tmpColor;
+        
     }
 }
