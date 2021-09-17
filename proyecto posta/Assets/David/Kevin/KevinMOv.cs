4d2883dbdbf3f03d3c88a106ab696e5359437c2e
davidfoigel <72942152+davidfoigel@users.noreply.github.com>
2021-09-17 15:43:13 -0300

    npc move fixed, anim incoming

--- proyecto posta/Assets/David/Kevin/KevinMOv.cs
+++ proyecto posta/Assets/David/Kevin/KevinMOv.cs
@@ -18,6 +18,7 @@ public class KevinMOv : MonoBehaviour
     float movX;
     float movZ;
     int estadoMov = 1;
+    public int NPC;
 
 
     Animator anim; 
@@ -26,6 +27,7 @@ public class KevinMOv : MonoBehaviour
     {
        // MoveToThisPoint(new Vector3(Fin.transform.position.x, Fin.transform.position.y, Fin.transform.position.z));
         anim = this.GetComponent<Animator>();
+        anim.SetInteger("NPC", NPC);
     }
 
     void Update(){
@@ -66,12 +68,15 @@ public class KevinMOv : MonoBehaviour
                     movX = -velocity;
                     transform.localScale = new Vector3(-0.8314339f, transform.localScale.y, transform.localScale.z);
                 }
+                
             }
-            if(z <= 0.5f && x <= 0.5f && z>= -0.5f && x >= -0.5f){
+            if(z <= percent && x <= percent && z>= -percent && x >= -percent){
                 move = false;
                 anim.SetBool("MovingZ", false);
                 anim.SetBool("MovingX", false);
                 anim.SetBool("Idle", true);
+                movX = 0;
+                Debug.Log("finish");
             }
             transform.Translate(movX,0,movZ);
             
