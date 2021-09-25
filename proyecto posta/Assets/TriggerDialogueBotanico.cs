b4b54a1caf5461127865e58ecc5ca72e8615f829
ianco99 <71154684+ianco99@users.noreply.github.com>
2021-09-25 13:15:31 -0300

    ,,

---
+++ proyecto posta/Assets/TriggerDialogueBotanico.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDialogueBotanico : MonoBehaviour
+{
+    GameObject Kevin;
+    bool used = false;
+
+    private void Start()
+    {
+        Kevin = GameObject.Find("KEVIN");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!used)
+        {
+            if (other.tag == "Player")
+            {
+                Kevin.GetComponent<Dialogue>().Talk("Jardin.txt", 1, 3);
+                used = !used;
+            }
+        }
+        
+    }
+}
