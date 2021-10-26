5354964400c9316e9c97f49b730b156b17142639
ianco99 <71154684+ianco99@users.noreply.github.com>
2021-10-26 19:40:09 -0300

    Bastante avance con void

---
+++ proyecto posta/Assets/triggerVoid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class triggerVoid : MonoBehaviour
+{
+    [SerializeField] Transform councilPos;
+    [SerializeField] GameObject Council;
+    [SerializeField] Transform cameraPos;
+    private void OnTriggerEnter(Collider other)
+    {
+        gameManager.instance.UpdateGameState(GameState.Dialogue);
+        GameObject.FindGameObjectWithTag("Player").GetComponent<movementReflect>().enabled = false;
+        GameObject playCam = GameObject.Find("PlayCam");
+        float zz = GameObject.FindGameObjectWithTag("Player").transform.position.z;
+        cameraPos.position = new Vector3(cameraPos.position.x, cameraPos.position.y, zz);
+        playCam.GetComponent<CinemachineVirtualCamera>().Follow = cameraPos;
+        playCam.GetComponent<CinemachineVirtualCamera>().LookAt = cameraPos;
+        float z = GameObject.FindGameObjectWithTag("Player").transform.position.z;
+        councilPos.position = new Vector3(councilPos.position.x, councilPos.position.y, z);
+        Instantiate(Council, councilPos.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
